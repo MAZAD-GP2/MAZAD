b048a7e63476898bc1303a46f40eec6f075356cc
MusaabAlfalahi <mosaab.dulimi@yahoo.com>
2024-02-23 23:23:24 +0300

    install dependencies

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000);
