1571aa7627a4ce6b593c0d7ad78e913fbb2d624a
MusaabAlfalahi <alfalahimusaab@gmail.com>
2024-02-25 19:59:21 +0300

    user model

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/', router);
 
 app.listen(process.env.PORT,() => {
---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+const Sequelize = require("sequelize").Sequelize;
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { is: /^(07[789]\d{7})$/ },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  }
+});
+
+module.exports = User;
