7c844580c9d409cc350ec1a635f06388adbae097
MusaabAlfalahi <alfalahimusaab@gmail.com>
2024-02-25 20:14:10 +0300

    user route

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const routes = require("./userRoutes");
 
-router.get("/user", routes.getAllUsers);
+router.route('/user').get(routes.getAllUsers);
 
 module.exports = router;
