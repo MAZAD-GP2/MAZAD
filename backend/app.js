804ee5d138f6e60ee479a37c43a636dfbbd2bfa7
MusaabAlfalahi <alfalahimusaab@gmail.com>
2024-02-25 19:39:20 +0300

    files structure

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const database = require("./config/database");
+const router = require("./routes/allRoutes");
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/', router);
 
 app.listen(process.env.PORT,() => {
   console.log(`app listenting to port ${process.env.PORT}`);
