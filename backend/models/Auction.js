8755c20759580eb9de60de34bfc8dfc6f66ba36d
Waleed <waleedabdulhadi4@gmail.com>
2024-02-28 00:58:38 +0300

    category and auction

---
+++ backend/models/Auction.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize").Sequelize;
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Auction = sequelize.define("Auction", {
+	id: {
+		type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+	},
+	startTime: {
+		type: DataTypes.DATE,
+		allowNull: false
+	},
+	finishTime: {
+		type: DataTypes.DATE,
+		allowNull: false
+	},
+	highestBid: {
+		type: DataTypes.INTEGER,
+		defaultValue: 0
+	}
+});
+
+module.exports = Auction;
