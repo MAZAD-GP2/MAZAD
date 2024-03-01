ca8e1c306049821ec84ab8db028322009c60effd
MusaabAlfalahi <alfalahimusaab@gmail.com>
2024-03-01 23:13:44 +0300

    adding missing try catch blocks in register

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const routes = require("./userRoutes");
 
-router.route('/user').get(routes.getAllUsers).post(routes.register);
+router.route('/user').get(routes.getAllUsers);
+router.route('/user/register').post(routes.register);
+// router.route('/user/login').post(routes.login);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -25,46 +25,48 @@ module.exports.getUserById = async (req, res) => {
 };
 
 module.exports.register = async (req, res) => {
-  const { username, password, email, phoneNumber } = req.body;
+  try {
+    const { username, password, email, phoneNumber } = req.body;
 
-  const usernameExists = await User.findOne({
-    where: { username: username },
-  });
-  if (usernameExists) {
-    throw new Error("Username already used");
+    const usernameExists = await User.findOne({
+      where: { username: username },
+    });
+    if (usernameExists) {
+      throw new Error("Username already used");
+    }
+    const emailExists = await User.findOne({
+      where: { email: email },
+    });
+    if (emailExists) {
+      throw new Error("Email already used");
+    }
+    const phoneNumberExsits = await User.findOne({
+      where: { phoneNumber: phoneNumber },
+    });
+    if (phoneNumberExsits) {
+      throw new Error("PhoneNumber already used");
+    }
+
+    hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.BCRYPT_SALT)
+    );
+
+    let newUser = await User.create({
+      username,
+      password: hashedPassword,
+      email,
+      phoneNumber,
+    });
+
+    const token = await generateJWT({
+      email: newUser.email,
+      id: newUser.id,
+      role: newUser.isAdmin,
+    });
+
+    return res.status(201).json({ ...newUser.dataValues, token });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
-  const emailExists = await User.findOne({
-    where: { email: email },
-  });
-  if (emailExists) {
-    throw new Error("Email already used");
-  }
-  const phoneNumberExsits = await User.findOne({
-    where: { phoneNumber: phoneNumber },
-  });
-  if (phoneNumberExsits) {
-    throw new Error("PhoneNumber already used");
-  }
-
-  hashedPassword = await bcrypt.hash(
-    password,
-    parseInt(process.env.BCRYPT_SALT)
-  );
-
-  let newUser = await User.create({
-    username,
-    password: hashedPassword,
-    email,
-    phoneNumber,
-  });
-
-  const token = await generateJWT({
-    email: newUser.email,
-    id: newUser.id,
-    role: newUser.isAdmin,
-  });
-
-  let result = {...newUser.dataValues, token};
-
-  return res.status(201).json(result);
 };
