fd397613d67301d66f8dd5c8eabeda4143d012c6
MoQaoud <hannon.uni@gmail.com>
2024-03-11 01:14:29 +0300

    fix some frontend stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import Login from "./pages/auth/Login.jsx";
 import Home from "./pages/Home.jsx";
 import ForgotPassword from "./pages/auth/ForgotPassword.jsx";
 import ResetPassword from "./pages/auth/ResetPassword.jsx";
+
 function App() {
+
   return (
     <>
       <Routes>
@@ -13,6 +15,7 @@ function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/forgot-password" element={<ForgotPassword />} />
         <Route exact path="/reset-password" element={<ResetPassword />} />
+        {/* <Route exact path="/admin" element={<ResetPassword />} /> */}
       </Routes>
     </>
   );
--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState, useEffect } from "react";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import axios from "axios";
 
 const Navbar = () => {
@@ -11,7 +11,7 @@ const Navbar = () => {
     const fetchItems = async () => {
       try {
         const response = await axios.get("http://localhost:3000/category");
-        setCategories(response.data);  
+        setCategories(response.data);
       } catch (err) {
         console.error(err);
       } finally {
@@ -23,61 +23,47 @@ const Navbar = () => {
   }, []);
   return (
     <div className="bg-gradient pb-4" style={{ padding: "0px" }}>
-      <nav className="navbar d-flex bg-primary shadow navbar-expand-lg navbar-light bg-light p-1 mb-0">
-        <h4 className="p-lg-3">
-          <a href="/" style={{ textDecoration: "none" }}>
+      <div className="position-relative d-flex align-items-start justify-content-between bg-primary shadow bg-light p-3 mb-0">
+        <h4 className="p-md-2 col-auto">
+          <h3 className="">
             <i className="text-white user-select-none ">_MAZAD_</i>
-          </a>
+          </h3>
         </h4>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse d-flex flex-row justify-content-center p-lg-1 gap-2"
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav mr-auto"></ul>
+        <div className="d-flex flex-row justify-content-center p-lg-1 gap-2 col-lg-5 col-md-8 col-sm-12" id="divbarSupportedContent">
+          
 
-          <form className="form-inline my-2 my-lg-0 w-50">
+          <form className="d-flex flex-row gap-3 w-100">
             <input
               className="form-control rounded-4 w-200 p-20"
               type="search"
               placeholder="Search Items, tags or categories"
               aria-label="Search"
             />
+            <button
+              className="btn btn-secondary rounded-4 col-auto"
+              type="submit"
+            >
+              Start a new Mazad
+            </button>
           </form>
-          <button
-            className="btn btn-secondary my-2 my-sm-0 rounded-4"
-            type="submit"
-          >
-            Search
-          </button>
         </div>
-        <div id="signup-container" className="d-flex">
+        <div id="signup-container" className="d-flex gap-3">
           <a
             href="/login"
             style={{ color: "#F7CCAC", opacity: "90%" }}
-            className="mx-2"
+            className="col-auto"
           >
             Log In
           </a>
           <a
             href="/register"
             style={{ color: "#F7CCAC", opacity: "90%" }}
-            className="mx-2"
+            className="col-auto"
           >
             Sign Up
           </a>
         </div>
-      </nav>
+      </div>
 
       {/* Second Navigation Bar with Dropdown */}
       <nav className="navbar d-flex bg-primary shadow navbar-expand-lg navbar-light bg-light mb-5 p-1 mt-0">
@@ -89,14 +75,16 @@ const Navbar = () => {
           >
             Home
           </a>
-          <div role="button"
-              id="categoriesDropdown"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false" 
-              style={{ color: "#F7CCAC", opacity: "90%" }}
-              >
-            <NavDropdown title="Categories" id="basic-nav-dropdown"   >{/*   style={{zIndex:'-1'}}   */}
+          <div
+            role="button"
+            id="categoriesDropdown"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            style={{ color: "#F7CCAC", opacity: "90%" }}
+          >
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              {/*   style={{zIndex:'-1'}}   */}
               {categories.length > 0 ? (
                 categories.map((category) => (
                   <NavDropdown.Item
