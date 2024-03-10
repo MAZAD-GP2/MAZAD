823ea912a9f259600981d32af28a329a37e9b8b0
yousefShaheen63 <yousefshaheen455@gmail.com>
2024-03-10 21:06:00 +0300

    forgot to import the route

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userRoutes = require("./userRoutes");
 const itemRoutes = require("./itemRoute");
+const categoryRoutes = require("./categoryRoutes");
 const verifyToken = require("../middlewares/verfiytoken");
 const checkAdmin = require("../middlewares/checkAdmin");
-const { validateUserCreation, validateUserUpdate } = require("../utils/validators/userValidator");
+const {
+  validateUserCreation,
+  validateUserUpdate,
+} = require("../utils/validators/userValidator");
 const { validateItemCreation } = require("../utils/validators/itemValidator");
 
 router.route("/user").get(verifyToken, checkAdmin, userRoutes.getAllUsers);
@@ -12,14 +16,20 @@ router.route("/user/register").post(validateUserCreation, userRoutes.register);
 router.route("/user/login").post(userRoutes.login);
 router.route("/user/forgot-password").post(userRoutes.forgotPassword);
 router.route("/user/reset-password").post(userRoutes.resetPassword);
-router.route("/user/update").put(verifyToken, validateUserUpdate, userRoutes.updateUser);
-router.route("/user/delete/:id").delete(verifyToken, checkAdmin, userRoutes.deleteUser);
+router
+  .route("/user/update")
+  .put(verifyToken, validateUserUpdate, userRoutes.updateUser);
+router
+  .route("/user/delete/:id")
+  .delete(verifyToken, checkAdmin, userRoutes.deleteUser);
 
-router.route("/item/create").post(verifyToken, validateItemCreation, itemRoutes.createItem);
+router
+  .route("/item/create")
+  .post(verifyToken, validateItemCreation, itemRoutes.createItem);
 router.route("/item/user").get(verifyToken, itemRoutes.getAllItemsByUserId);
 router.route("/item").get(itemRoutes.getAllItems);
 
-router.route('/category').get(categoryRoutes.getAllCategories);
-router.route('/category/create').post(categoryRoutes.createCategory)
+router.route("/category").get(categoryRoutes.getAllCategories);
+router.route("/category/create").post(categoryRoutes.createCategory);
 
 module.exports = router;
